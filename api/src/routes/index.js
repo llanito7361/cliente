aeb65bde49eafed48a22d908268e6ead7442ba54
llanito7361 <llanito.code.log@gmail.com>
2024-09-28 18:25:37 -0300

    asd

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,4 +8,9 @@ const router = Router()
 router.use('/games', gamesRouter) //funciona
 router.use('/genres', genresRouter) //funciona
 router.use('/platforms',platformsRouter ) //funciona
+
+router.get('/',(req,res)=>{
+    res.status(200).send('/estamos en el inicio, en / ')
+})
+
 module.exports = router
